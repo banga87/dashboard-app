8c9dcca4cfcacdc0f49668c2c29747e589006ea6
BingBong <angusroymcdonald@gmail.com>
2023-08-09 21:17:55 +1000

    styled the sidebar somewhat

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,26 +1,32 @@
 import styles from '../styles/Sidebar.module.css'
 import Link from 'next/link';
+import Image from 'next/image';
 
 
 const Sidebar = () => {
     return (
-        <div className={ styles.sidebar }>
-            <div>COMPANY LOGO</div>
-            <ul>
-                <li>
-                    <Link href="/">Home</Link>
+        <div className={ styles.navbar }>
+            <div className={ styles.logo }>
+                <h1>LOGO</h1>
+            </div>
+            <ul className={ styles.navbarNav }>
+                <li className={ styles.navbarItem }>
+                    <Link className={ styles.navbarLink } href="/">Home</Link>
                 </li>
-                <li>
-                    <Link href="/worker">Worker</Link>
+                <li className={ styles.navbarItem }>
+                    <Link className={ styles.navbarLink } href="/worker">Worker</Link>
                 </li>
-                <li>
-                    <Link href="/customer">Customer</Link>
+                <li className={ styles.navbarItem }>
+                    <Link className={ styles.navbarLink } href="/customer">Customer</Link>
                 </li>
-                <li>
-                    <Link href="/service">Service</Link>
+                <li className={ styles.navbarItem }>
+                    <Link className={ styles.navbarLink } href="/service">Service</Link>
                 </li>
-                <li>
-                    <Link href="/mobile">Mobile</Link>
+                <li className={ styles.navbarItem }>
+                    <Link className={ styles.navbarLink } href="/mobile">Mobile</Link>
+                </li>
+                <li className={ styles.navbarItem }>
+                    <Link className={ styles.navbarLink } href="/settings">Settings</Link>
                 </li>
             </ul>
         </div>
